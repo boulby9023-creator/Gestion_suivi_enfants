657c1de407ce2ec491f923ba781628e6a01d55b7
Aissata <kanouteaissata001@gmail.com>
2026-05-06 14:58:11 +0000

    correction2

--- src/main/java/Modele/Corporelle.java
+++ src/main/java/Modele/Corporelle.java
@@ -1,19 +1,21 @@
 package main.java.Modele;
-import java.util.Scanner;
+
+import java.util.Date;
+
 public class Corporelle {
     private int id;
     private  double poids;
     private double taille;
     private double imc;
-    private java.util.Date date;
-
-   
-
-    public Corporelle( int id, double poids, double taille, java.util.Date date) {
+    private java.util.Date date_mesure;
+    public Corporelle(int id, double poids, double taille, double imc, Date date_mesure) {
         this.id = id;
         this.poids = poids;
         this.taille = taille;
-        this.date = date;
+        this.imc = imc;
+        this.date_mesure = date_mesure;
+    }
+    public Corporelle() {
     }
     public int getId() {
         return id;
@@ -26,19 +28,30 @@ public class Corporelle {
     }
     public void setPoids(double poids) {
         this.poids = poids;
-       
     }
     public double getTaille() {
         return taille;
     }
     public void setTaille(double taille) {
         this.taille = taille;
-       
     }
-    public double getImc(double poids, double taille) {
-        imc = poids / (taille * taille);
+    public double getImc() {
         return imc;
- 
     }
+    public void setImc(double imc) {
+        this.imc = imc;
+    }
+    public java.util.Date getDate_mesure() {
+        return date_mesure;
+    }
+    public void setDate_mesure(java.util.Date date_mesure) {
+        this.date_mesure = date_mesure;
+    }
+    
+    
+
+
+
    
+
 }
--- src/main/java/Modele/Enfant.java
+++ src/main/java/Modele/Enfant.java
@@ -1,8 +1,5 @@
 package main.java.Modele;
 public class Enfant {
-    public static void main(String[] args) {
-        
-    }
 
     private int id;
     private String nom;
@@ -10,12 +7,14 @@ public class Enfant {
     private int age;
     private String sexe;
 
-    public Enfant(String nom, String prenom, int age, String sexe) {
+    public Enfant(int id, String nom, String prenom, int age, String sexe) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
         this.age = age;
         this.sexe = sexe;
+    }
+    public Enfant() {
     }
     public int getId() {
         return id;
