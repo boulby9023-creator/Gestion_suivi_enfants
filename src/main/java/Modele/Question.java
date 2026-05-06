cdfb21e8f96c93c72f03bf1a9425e9fac9e2ed72
ibrahima coulibaly <boulby9023@gmail.com>
2026-05-06 11:20:36 +0200

    ajout de la classe question

---
+++ src/main/java/Modele/Question.java
@@ -0,0 +1,10 @@
+package main.java.Modele;
+
+import main.java.enumeration.CapaciteEnum;
+
+public class Question {
+    private int id_quest;
+    private String contenu;
+    private int point;
+    private  CapaciteEnum capaciteEnum;
+}
