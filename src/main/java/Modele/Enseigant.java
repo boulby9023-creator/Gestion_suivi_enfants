808cc2f71bbd8e6ca3c88498e15ffb42c0233e22
med <seibacly@gmail.com>
2026-05-06 12:16:12 +0000

    classe enseignants

---
+++ src/main/java/Modele/Enseigant.java
@@ -0,0 +1,8 @@
+package main.java.Modele;
+
+public class Enseigant  extends Utilisateur{
+
+    public Enseigant(int id, String nom, String prenom, String tel, String mail, String password, String role){
+        super(id, nom, prenom, tel, mail, password, role);
+    }
+}
