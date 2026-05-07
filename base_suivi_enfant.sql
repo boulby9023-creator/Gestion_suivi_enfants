a9f760fa4b35c34a6ef32b3f58011541f8610e35
med <seibacly@gmail.com>
2026-05-07 09:38:14 +0000

    creation de la table enfant et admin

--- base_suivi_enfant.sql
+++ base_suivi_enfant.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE Suivi_enfant
+
+CREATE TABLE enfants (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     nom VARCHAR(20) NOT NULL,
+     prenom VARCHAR(20),
+     age INT,
+     sexe VARCHAR(15) CHECK (sexe IN ('homme', 'femme'))
+);
+
+CREATE TABLE admins (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+);
+
