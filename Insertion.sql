2754b95808b22e321ff0925b2f8cef98df8a736f
DIARRA Adama <ad5299019@gmail.com>
2026-05-07 15:47:04 +0000

    Adama_Insertion_modif

--- Insertion.sql
+++ Insertion.sql
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
+
 INSERT INTO recommendations (description,  date_recommandation, type) VALUES 
 ("Proposer à Amadou des exercices courts et interactifs de 10 à 15 minutes 
 afin d’améliorer sa concentration. Utiliser des jeux éducatifs de mémoire et 
 de logique avec un système d’encouragement après chaque activité réussie."
 , "2026-05-07", "Concentration et motivation") ;
-=======
-
-INSERT INTO recommandations (id_enfant, description, categorie, priorite) 
-VALUES 
-(1, 'Encourager des jeux de mémoire visuelle (memoire) 15 min par jour.', 'Cognitif', 'Haute'),
-(1, 'Pratiquer la lecture à voix haute pour améliorer la compréhension.', 'Intellectuel', 'Moyenne'),
-(2, 'Proposer des activités de dessin pour stimuler la gestion des émotions.', 'Émotionnel', 'Basse'),
-(3, 'Augmenter l\'apport en calcium suite à la dernière courbe de croissance.', 'Corporel', 'Haute');
->>>>>>> 90de4949130eadd9f8fc7b757b16c5dd49ba21bb
