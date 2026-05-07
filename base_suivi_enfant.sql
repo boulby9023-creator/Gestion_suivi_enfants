8bfcac94985ff61b53b247f75d25ca3a2f1f3f49
Aissata <kanouteaissata001@gmail.com>
2026-05-07 09:39:45 +0000

    creation table question et option

--- base_suivi_enfant.sql
+++ base_suivi_enfant.sql
@@ -1,1 +1,16 @@
-CREATE DATABASE Suivi_enfant
+CREATE DATABASE Suivi_enfant;
+
+CREATE TABLE Question (
+    id_Q INT PRIMARY KEY AUTO_INCREMENT,
+    enonce VARCHAR(255) NOT NULL,
+    pointure INT ,
+    typeCapacite ENUM('Logique', 'Memoire', 'Attention'),
+    delaiMax INT
+);
+
+CREATE TABLE Option (
+    id_O INT PRIMARY KEY AUTO_INCREMENT,
+    texte VARCHAR(255) NOT NULL,
+    estCorrect BOOLEAN,
+    FOREIGN KEY (question_id) REFERENCES Question(id)
+);
