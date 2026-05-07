402b7888ec0f1b237761d4f1b9653707f2fb0945
Oumar Kanadji <oumarbambakanadji@gmail.com>
2026-05-07 10:50:38 +0000

    modification

--- base_suivi_enfant.sql
+++ base_suivi_enfant.sql
@@ -1,1 +1,7 @@
-CREATE DATABASE Suivi_enfant
+CREATE DATABASE Suivi_enfant;
+ CREATE TABLE Recommandation (
+    id_re INT AUTO_INCREMENT PRIMARY KEY,
+    description TEXT NOT NULL,
+    date_recommandation DATE NOT NULL,
+    type VARCHAR(100) NOT NULL
+);
