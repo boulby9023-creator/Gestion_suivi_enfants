4d28b8053a7030811d37b504339334bf3352ac84
DIARRA Adama <ad5299019@gmail.com>
2026-05-07 09:31:00 +0000

    création_table_utilisateur

--- base_suivi_enfant.sql
+++ base_suivi_enfant.sql
@@ -1,1 +1,13 @@
-CREATE DATABASE Suivi_enfant
+CREATE DATABASE Suivi_enfant;
+
+USE Suivi_enfant;
+
+create table utilisateur(
+    id int primary key auto_increment,
+     nom varchar(35),
+    prenom varchar(40),
+    tel varchar(25),
+    mail varchar(45),
+    password varchar(35),
+    role varchar(20)
+    );
