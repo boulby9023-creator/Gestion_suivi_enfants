a92e43863752e27ecd3ecc53536bc7a1166230be
DIARRA Adama <ad5299019@gmail.com>
2026-05-08 11:29:58 +0000

    modif2

--- Insertion.sql
+++ Insertion.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO recommendations (description,  date_recommandation, type) VALUES 
+INSERT INTO recommendations (description,  date_recommandations, type_recommandations) VALUES 
 ("Il lui faut des exercices courts et interactifs de 10 à 15 minutes 
 afin d’améliorer sa concentration. Utiliser des jeux éducatifs de mémoire et 
 de logique avec un système d’encouragement après chaque activité réussie."
