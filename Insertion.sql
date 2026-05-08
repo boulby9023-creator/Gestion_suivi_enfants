ce68474f2bfbb2c9f1f0421dc888e53589b078af
Oumar Kanadji <oumarbambakanadji@gmail.com>
2026-05-08 10:47:11 +0000

    Merge branch 'master' of https://github.com/boulby9023-creator/Gestion_suivi_enfants

--- Insertion.sql
+++ Insertion.sql
@@ -5,17 +5,3 @@ VALUES
 (1, 'Pratiquer la lecture à voix haute pour améliorer la compréhension.', 'Intellectuel', 'Moyenne'),
 (2, 'Proposer des activités de dessin pour stimuler la gestion des émotions.', 'Émotionnel', 'Basse'),
 (3, 'Augmenter l\'apport en calcium suite à la dernière courbe de croissance.', 'Corporel', 'Haute');
-
-
-
-
-
-INSERT INTO recommandations (id_enfant, description, categorie, priorite) 
-VALUES 
-(6, 'Mettre en place des exercices de lecture syllabique avec support visuel pendant 15 minutes par jour.', 'Intellectuel', 'Haute'),
-
-(7, 'Pratiquer des activités de différenciation gauche/droite à travers des jeux interactifs et des consignes simples.', 'Cognitif', 'Haute'),
-
-(8, 'Encourager les activités de dessin, puzzles et logique visuelle afin de renforcer la confiance et les capacités de repérage.', 'Cognitif', 'Moyenne'),
-
-(9, 'Créer un environnement rassurant pour les exercices oraux afin de réduire l’anxiété scolaire et favoriser l’expression en classe.', 'Émotionnel', 'Haute');
