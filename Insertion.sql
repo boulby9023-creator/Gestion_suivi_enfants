02b8486cdaf2f8afcb1458140a7c0be67e89cdad
Oumar Kanadji <oumarbambakanadji@gmail.com>
2026-05-08 11:02:56 +0000

    insertion de recommendation

--- Insertion.sql
+++ Insertion.sql
@@ -4,3 +4,14 @@ INSERT INTO recommendations (description,  date_recommandation, type) VALUES
 afin d’améliorer sa concentration. Utiliser des jeux éducatifs de mémoire et 
 de logique avec un système d’encouragement après chaque activité réussie."
 , "2026-05-07", "Concentration et motivation") ;
+
+
+INSERT INTO recommandations (id_enfant, description, categorie, priorite) 
+VALUES 
+(6, 'Mettre en place des exercices de lecture syllabique avec support visuel pendant 15 minutes par jour.', 'Intellectuel', 'Haute'),
+
+(7, 'Pratiquer des activités de différenciation gauche/droite à travers des jeux interactifs et des consignes simples.', 'Cognitif', 'Haute'),
+
+(8, 'Encourager les activités de dessin, puzzles et logique visuelle afin de renforcer la confiance et les capacités de repérage.', 'Cognitif', 'Moyenne'),
+
+(9, 'Créer un environnement rassurant pour les exercices oraux afin de réduire l’anxiété scolaire et favoriser l’expression en classe.', 'Émotionnel', 'Haute');
