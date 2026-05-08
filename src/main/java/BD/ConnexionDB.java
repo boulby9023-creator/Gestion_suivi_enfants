2ddcfe09d8e7991cb3b4dc2968be3f065a15e6d3
ibrahima coulibaly <boulby9023@gmail.com>
2026-05-08 18:33:36 +0200

    m

--- src/main/java/BD/ConnexionDB.java
+++ src/main/java/BD/ConnexionDB.java
@@ -61,5 +61,37 @@ public class ConnexionDB {
  */
 
 
+    private static String url = "jdbc:mysql://localhost:3306/suivi_enfant";
+    private static String utilisateur = "root";
+    private static String mot_de_passe = "root";
+
+    public static Connection getConexion() {
+        try {
+            Connection conexion = DriverManager.getConnection(url, utilisateur, mot_de_passe);
+            System.out.println("Connecté à la base de données !");
+            return conexion;
+        } catch (Exception e) {
+            System.out.println("Erreur de connexion : " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection con = ConnexionDB.getConexion();
+            if (con != null) {
+                Statement pont = con.createStatement();
+                String sql = "INSERT INTO capacite VALUES (NULL, 'Emotionnelle')";
+                int lignesAffectees = pont.executeUpdate(sql);
+
+                System.out.println("Insertion réussie, lignes modifiées : " + lignesAffectees);
+                pont.close();
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     
 }
