b85f0e1d2d2562d2aa6cb19eb2ebc2ac5ad23389
boulby9023-creator <boulby9023@gmail.com>
2026-05-08 16:53:11 +0200

    Delete obsolete INSERT statements from Insertion.sql
    Removed outdated recommendation entries from the database.

--- Insertion.sql
+++ Insertion.sql
@@ -1,9 +1,3 @@
-INSERT INTO recommendations (description,  date_recommandation, type) VALUES 
-("Il lui faut des exercices courts et interactifs de 10 à 15 minutes 
-afin d’améliorer sa concentration. Utiliser des jeux éducatifs de mémoire et 
-de logique avec un système d’encouragement après chaque activité réussie."
-, "2026-05-07", "Concentration et motivation") ;
-
 INSERT INTO recommendations (description,  date_recommandation, id_capacite) VALUES 
 ("Proposez-lui des jeux de stratégie comme les échecs ou des casse-têtes pour muscler sa capacité de planification logique.", 
 "2026-05-07",
@@ -23,5 +17,3 @@ INSERT INTO Capacites (type_capacite) VALUES
 ('Émotionnel'),
 ('Mental'),
 ('Corporel');
-INSERT INTO recommandations (descriptions, date_recommandations, type_recommandations, id_evaluations)
-VALUES ("Ismaël, tes oreilles sont des championnes qui connaissent déjà tout, laisse maintenant le temps à tes mains de devenir aussi fortes que tes oreilles, petit à petit.","2026-05-08","Logique",null);
