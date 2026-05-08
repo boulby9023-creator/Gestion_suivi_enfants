28d3525f048d75fdf68c27fa27238dd3a017d80e
Moulaye amadou dagnon <moulayedagnon@gmail.com>
2026-05-08 16:50:47 +0000

    add my connexion bdd

--- src/main/java/BD/Connexion.java
+++ src/main/java/BD/Connexion.java
@@ -1,18 +1,46 @@
+
+
 package main.java.BD;
+
 import java.sql.*;
 
 public class Connexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/gestion_suivi_enfant";
+   private static String url = "jdbc:mysql://localhost:3306/Suivi_enfant?useSSL=false&serverTimezone=UTC";
+   private static String username = "root";
+   private  static String mot_de_passe = "Moul@9460";
 
-    private static final String USER ="root";
-    private static final String PASSWORD = "Obkanadji223";
-    public static void main(String[] args) {
+   public static Connection getConnexion(){
+    Connection connexion = null;
         try {
-            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Connexion réussie !");
-            connection.close();
+             connexion =  DriverManager.getConnection(url, username, mot_de_passe);
+            System.out.println("Connexion etablis avec la base de données");
+        } catch ( java.lang.Exception e) {
+            System.out.println("Il y a un probleme au niveau de la connexion" + e.getMessage());
+        }
+        return connexion ;
+   }   
+
+
+
+     public static void main(String[] args) {
+        try {
+            Connection con = Connexion.getConnexion();
+            if (con != null) {
+                Statement pont = con.createStatement();
+                String sql = "INSERT INTO capacites VALUES (NULL, 'Emotionnelle')";
+                int lignesAffectees = pont.executeUpdate(sql);
+
+                System.out.println("Insertion réussie, lignes modifiées : " + lignesAffectees);
+                pont.close();
+                con.close();
+            }
         } catch (SQLException e) {
-            System.out.println("Erreur de connexion : " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
+
+
+
+
+
