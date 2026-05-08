312c83d2724720d6a02beb0df55fb9c20842f1e4
ibrahima coulibaly <boulby9023@gmail.com>
2026-05-08 13:51:14 +0200

    m

--- Insertion.sql
+++ Insertion.sql
@@ -1,10 +1,3 @@
-INSERT INTO recommandations (descriptions, date_recommandations, type_recommandations, id_evaluations) 
-VALUES 
-('Encourager des jeux de mémoire visuelle (mémoire) 15 min par jour.', '2026-05-08', 'Memoire', 1),
-('Pratiquer la lecture à voix haute pour améliorer la concentration.', '2026-05-08', 'Attention', 1),
-('Utiliser des puzzles pour stimuler le raisonnement.', '2026-05-08', 'Logique', 2),
-('Exercices de mémorisation des chiffres après chaque séance.', '2026-05-08', 'Memoire', 3);
-
 INSERT INTO Capacites (type_capacite) VALUES 
 ('Cognitif'),
 ('Intellectuel'),
@@ -12,3 +5,4 @@ INSERT INTO Capacites (type_capacite) VALUES
 ('Émotionnel'),
 ('Mental'),
 ('Corporel');
+
