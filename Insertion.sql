54fe6d250f9549eff97249d5a01577e7a0798253
med <seibacly@gmail.com>
2026-05-08 11:59:09 +0000

    ajout du gitignore

--- Insertion.sql
+++ Insertion.sql
@@ -6,12 +6,12 @@ VALUES
 (2, 'Proposer des activités de dessin pour stimuler la gestion des émotions.', 'Émotionnel', 'Basse'),
 (3, "Augmenter l\'apport en calcium suite à la dernière courbe de croissance.", 'Corporel', 'Haute');
 
-INSERT INTO recommandation(type, description, date) VALUES
-('Attention', 'Faire des exercices de concentration pendant 15 minutes par jour', '2026-05-07'),
-('Logique', 'Encourager la lecture d’histoires adaptées à son âge', '2026-05-07'),
-('Memoire', 'Féliciter l’enfant après chaque activité réussie', '2026-05-07'),
-('Mémoire', 'Utiliser des jeux de mémoire visuelle deux fois par semaine', '2026-05-07'),
-('Attention', 'Favoriser les activités de groupe avec d’autres enfants', '2026-05-07');
+INSERT INTO recommandation(id_capacite, description, date) VALUES
+(2, 'Faire des exercices de concentration pendant 15 minutes par jour', '2026-05-07'),
+(3, 'Encourager la lecture d’histoires adaptées à son âge', '2026-05-07'),
+(5, 'Féliciter l’enfant après chaque activité réussie', '2026-05-07'),
+(5, 'Utiliser des jeux de mémoire visuelle deux fois par semaine', '2026-05-07'),
+(2, 'Favoriser les activités de groupe avec d’autres enfants', '2026-05-07');
 
 INSERT INTO Capacites (type_capacite) VALUES
 ('Cognitif'),
