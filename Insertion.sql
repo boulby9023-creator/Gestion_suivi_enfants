cc4c3f278282211221bc4a94034235d01cb41ad4
ibrahima coulibaly <boulby9023@gmail.com>
2026-05-08 13:53:06 +0200

    suppression dans le fichier insertion

--- Insertion.sql
+++ Insertion.sql
@@ -1,5 +1,3 @@
-
-
 INSERT INTO recommendations (description,  date_recommandation, id_capacite) VALUES 
 ("Proposez-lui des jeux de stratégie comme les échecs ou des casse-têtes pour muscler sa capacité de planification logique.", 
 "2026-05-07",
