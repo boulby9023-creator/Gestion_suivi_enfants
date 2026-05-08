74c939503e6d9680ce7e5c02c4f16819bd94b847
Cheick <cheikhsacko179@gmail.com>
2026-05-08 11:52:56 +0000

    modification table recommadations sql

--- Insertion.sql
+++ Insertion.sql
@@ -8,11 +8,11 @@ VALUES
 
 
 
-INSERT INTO recommandations (id_enfant, description, categorie, priorite) 
+INSERT INTO recommandations ( id_capacite, description, priorite) 
 VALUES 
-(1, 'Améliorer la mémoire en utilisant des jeux de répétition de séquences (mots ou images) 10 à 15 minutes par jour.', 'Cognitif', 'Haute'),
-(2, 'Renforcer la confiance en soi en valorisant les réussites de l’enfant au quotidien.', 'Émotionnel', 'Haute'),
-(3, 'Surveiller la croissance et encourager une alimentation riche en protéines et vitamines.', 'Corporel', 'Haute'),
+( 1, 'Améliorer la mémoire en utilisant des jeux de répétition de séquences (mots ou images) 10 à 15 minutes par jour.', 'Haute'),
+( 4, 'Renforcer la confiance en soi en valorisant les réussites de l’enfant au quotidien.', 'Haute'),
+( 6, 'Surveiller la croissance et encourager une alimentation riche en protéines et vitamines.', 'Haute'),
 
 
 ('Encourager des jeux de mémoire visuelle (mémoire) 15 min par jour.', '2026-05-08', 'Memoire', 1),
