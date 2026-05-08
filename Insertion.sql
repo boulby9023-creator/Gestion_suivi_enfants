62949df29698d9a0e5578bd0cc4f8a56393b5780
med <seibacly@gmail.com>
2026-05-08 12:11:24 +0000

    modification

--- Insertion.sql
+++ Insertion.sql
@@ -1,17 +1,4 @@
 
-INSERT INTO recommandations (id_enfant, description, categorie, priorite)
-VALUES
-(1, 'Encourager des jeux de mémoire visuelle (memoire) 15 min par jour.', 'Cognitif', 'Haute'),
-(1, 'Pratiquer la lecture à voix haute pour améliorer la compréhension.', 'Intellectuel', 'Moyenne'),
-(2, 'Proposer des activités de dessin pour stimuler la gestion des émotions.', 'Émotionnel', 'Basse'),
-(3, "Augmenter l\'apport en calcium suite à la dernière courbe de croissance.", 'Corporel', 'Haute');
-
-INSERT INTO recommandation(descriptions, date_recommandations,  id_capacite) VALUES
-( 'Faire des exercices de concentration pendant 15 minutes par jour', '2026-05-07',2),
-( 'Encourager la lecture d’histoires adaptées à son âge', '2026-05-07',3),
-( 'Féliciter l’enfant après chaque activité réussie', '2026-05-07', 5),
-( 'Utiliser des jeux de mémoire visuelle deux fois par semaine', '2026-05-07',5),
-( 'Favoriser les activités de groupe avec d’autres enfants', '2026-05-07',2);
 
 INSERT INTO Capacites (type_capacite) VALUES 
 ('Cognitif'),
