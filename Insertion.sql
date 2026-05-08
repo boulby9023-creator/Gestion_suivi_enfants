62ccec1cae49c2025050f37bdfaaeca01105d104
ibrahima coulibaly <boulby9023@gmail.com>
2026-05-08 14:20:10 +0200

    m

--- Insertion.sql
+++ Insertion.sql
@@ -1,7 +1,22 @@
-INSERT INTO recommendations (description,  date_recommandation, id_capacite) VALUES 
+INSERT INTO recommendations (descriptions,  date_recommandation, id_capacite) VALUES 
 ("Proposez-lui des jeux de stratégie comme les échecs ou des casse-têtes pour muscler sa capacité de planification logique.", 
 "2026-05-07",
 1);
+
+
+
+
+INSERT INTO recommandations( descriptions, date_recommandations,  id_capacite )
+VALUES ( 'Mettre en place des exercices courts et dynamiques avec rappels fréquents.', '2026-05-07', 7),
+        ('Mettre en place un système de récompense afin de motiver l’enfant à terminer ses activités.','2026-05-07', 8),
+        ('Encourager l’enfant à exprimer ses émotions à travers des activités artistiques.','2026-05-07', 4),
+        ('Proposer des activités physiques régulières pour améliorer la santé mentale et physique de l’enfant.','2026-05-07', 5)
+        ('Utiliser des jeux de rôle pour développer les compétences sociales et émotionnelles de l’enfant.','2026-05-07', 4)
+        ('Introduire des exercices de respiration et de méditation pour aider à gérer le stress et l’anxiété.','2026-05-07', 5),
+        ('Encourager la lecture quotidienne pour stimuler la créativité et l’imagination de l’enfant.','2026-05-07', 3),
+        ('Proposer des activités de résolution de problèmes pour renforcer les capacités cognitives de l’enfant.','2026-05-07', 1),
+        ('Mettre en place un environnement d’apprentissage structuré pour aider à améliorer la concentration de l’enfant.','2026-05-07',  7)
+;
 
 
 
@@ -15,17 +30,3 @@ INSERT INTO Capacites (type_capacite) VALUES
 ('Attention'),
 ('Motivation')
 ;
-
-
-INSERT INTO recommandations( description, date_recommandations,  id_capacite )
-VALUES ( 'Mettre en place des exercices courts et dynamiques avec rappels fréquents.', '2026-05-07', 7),
-        ('Mettre en place un système de récompense afin de motiver l’enfant à terminer ses activités.','2026-05-07', 8),
-        ('Encourager l’enfant à exprimer ses émotions à travers des activités artistiques.','2026-05-07', 4),
-        ('Proposer des activités physiques régulières pour améliorer la santé mentale et physique de l’enfant.','2026-05-07', 5)
-        ('Utiliser des jeux de rôle pour développer les compétences sociales et émotionnelles de l’enfant.','2026-05-07', 4)
-        ('Introduire des exercices de respiration et de méditation pour aider à gérer le stress et l’anxiété.','2026-05-07', 5),
-        ('Encourager la lecture quotidienne pour stimuler la créativité et l’imagination de l’enfant.','2026-05-07', 3),
-        ('Proposer des activités de résolution de problèmes pour renforcer les capacités cognitives de l’enfant.','2026-05-07', 1),
-        ('Mettre en place un environnement d’apprentissage structuré pour aider à améliorer la concentration de l’enfant.','2026-05-07',  7)
-;
-
