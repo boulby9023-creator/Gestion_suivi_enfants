c8187c7ccdf89e9137aaaac2a2c7a90685299341
Al Banou Sylla <albanou100www@gmail.com>
2026-05-08 11:31:17 +0000

    ajout de la recommandation d'Al Banou Sylla

--- Insertion.sql
+++ Insertion.sql
@@ -3,7 +3,8 @@ VALUES
 ('Encourager des jeux de mémoire visuelle (mémoire) 15 min par jour.', '2026-05-08', 'Memoire', 1),
 ('Pratiquer la lecture à voix haute pour améliorer la concentration.', '2026-05-08', 'Attention', 1),
 ('Utiliser des puzzles pour stimuler le raisonnement.', '2026-05-08', 'Logique', 2),
-('Exercices de mémorisation des chiffres après chaque séance.', '2026-05-08', 'Memoire', 3);
+('Exercices de mémorisation des chiffres après chaque séance.', '2026-05-08', 'Memoire', 3),
+("Ismaël, tes oreilles sont des championnes qui connaissent déjà tout, laisse maintenant le temps à tes mains de devenir aussi fortes que tes oreilles, petit à petit.","2026-05-08","Logique",null);
 
 INSERT INTO Capacites (type_capacite) VALUES 
 ('Cognitif'),
--- src/main/java/BD/Connexion.java
+++ src/main/java/BD/Connexion.java
@@ -1,7 +1,22 @@
 package main.java.BD;
 import java.sql.*;
 
-public class Connexion {
-    //DriverManager.getConnection()
+public class Connexion  {
+
+    static {
+        System.out.println("Initialisation faite");
+    }
+    public static Connexion getConnexion() throws Exception {
+
+        return (Connexion) DriverManager.getConnection("jdbc:mysql://localhost:3306/suivi_enfant","root","root");
+    }
+
+    static void main(String[] args) throws Exception{
+        Connection conn= (Connection) Connexion.getConnexion();
+        Statement executeur=conn.createStatement();
+        ResultSet donnees=executeur.executeQuery("INSERT INTO recommandations (id_enfant, description, categorie, priorite) \n" +
+                "VALUES \n" +
+                "(1, 'Encourager des jeux de mémoire visuelle (memoire) 15 min par jour.', 'Cognitif', 'Haute'),");
+    }
 
 }
