198dc2b346fd247228cac840156eeb707a76f032
Cheick <cheikhsacko179@gmail.com>
2026-05-08 10:36:11 +0000

    ajout des recommadations sql

--- Insertion.sql
+++ Insertion.sql
@@ -5,3 +5,12 @@ VALUES
 (1, 'Pratiquer la lecture à voix haute pour améliorer la compréhension.', 'Intellectuel', 'Moyenne'),
 (2, 'Proposer des activités de dessin pour stimuler la gestion des émotions.', 'Émotionnel', 'Basse'),
 (3, 'Augmenter l\'apport en calcium suite à la dernière courbe de croissance.', 'Corporel', 'Haute');
+
+
+
+INSERT INTO recommandations (id_enfant, description, categorie, priorite) 
+VALUES 
+(1, 'Améliorer la mémoire en utilisant des jeux de répétition de séquences (mots ou images) 10 à 15 minutes par jour.', 'Cognitif', 'Haute'),
+(2, 'Renforcer la confiance en soi en valorisant les réussites de l’enfant au quotidien.', 'Émotionnel', 'Haute'),
+(3, 'Surveiller la croissance et encourager une alimentation riche en protéines et vitamines.', 'Corporel', 'Haute'),
+
