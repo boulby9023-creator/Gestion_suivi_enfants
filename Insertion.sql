31619e539b25beae57b366388818b9c2794d3898
med <seibacly@gmail.com>
2026-05-08 11:01:37 +0000

    correction des insertions

--- Insertion.sql
+++ Insertion.sql
@@ -7,8 +7,8 @@ VALUES
 (3, "Augmenter l\'apport en calcium suite à la dernière courbe de croissance.", 'Corporel', 'Haute');
 
 INSERT INTO recommandation(type, description, date) VALUES
-('Concentration', 'Faire des exercices de concentration pendant 15 minutes par jour', '2026-05-07'),
-('Lecture', 'Encourager la lecture d’histoires adaptées à son âge', '2026-05-07'),
-('Motivation', 'Féliciter l’enfant après chaque activité réussie', '2026-05-07'),
+('Attention', 'Faire des exercices de concentration pendant 15 minutes par jour', '2026-05-07'),
+('Logique', 'Encourager la lecture d’histoires adaptées à son âge', '2026-05-07'),
+('Memoire', 'Féliciter l’enfant après chaque activité réussie', '2026-05-07'),
 ('Mémoire', 'Utiliser des jeux de mémoire visuelle deux fois par semaine', '2026-05-07'),
-('Social', 'Favoriser les activités de groupe avec d’autres enfants', '2026-05-07');
+('Attention', 'Favoriser les activités de groupe avec d’autres enfants', '2026-05-07');
