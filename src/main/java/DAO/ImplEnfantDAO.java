9c4732b2990e814d71b8042d9d56469ae8396c5e
Hamathx999 <hamath.o.diallo18@gmail.com>
2026-05-11 14:47:51 +0000

    ajout de la liaison ajout du fichier ImpEnfantDao

---
+++ src/main/java/DAO/ImplEnfantDAO.java
@@ -0,0 +1,53 @@
+package main.java.DAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import static java.sql.Types.INTEGER;
+import java.util.List;
+
+import main.java.BD.Connexion;
+import main.java.Modele.Enfant;
+
+public class ImplEnfantDAO implements Repository<Enfant, Integer> {
+
+    @Override
+    public void save(Enfant entity) {
+        try{
+        Connection con = Connexion.getConexion();
+        String sql = "INSERT INTO Enfant(id_enfants, nom, prenom, date_naissance, sexe, id_activites, id_parent) Values (?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement stm1 = con.prepareStatement(sql);
+        stm1.setNull(1, INTEGER);
+        stm1.setString(2, entity.getNom());
+        stm1.setString(3, entity.getPrenom());
+        stm1.setDate(4, java.sql.Date.valueOf(String.valueOf(entity.getDate_naissance()))); 
+        stm1.setString(5, entity.getSexe());
+        stm1.setInt(6, entity.getId());
+        stm1.setInt(7, entity.getid_parent());
+
+        stm1.execute();
+        con.close();
+        }
+        catch(SQLException e){
+            System.out.println("Une erreur est survenue lors de l'insertion !");
+        }
+    }
+
+    @Override
+    public Enfant findById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public List<Enfant> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public void delete(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+    
+}
