1b7b340e52b3be41f240976aa94de9764acf98bc
Famory_Josue_Sissoko <famoryjosuesissoko@gmail.com>
2026-05-12 08:34:22 +0000

    parametre update

--- src/main/java/Modele/Questionquiz.java
+++ src/main/java/Modele/Questionquiz.java
@@ -3,4 +3,31 @@ package main.java.Modele;
 public class Questionquiz {
     private int id_question;
     private int id_quiz;
+
+    public Questionquiz() {
+    }
+    
+    public Questionquiz(int id_question, int id_quiz) {
+        this.id_question = id_question;
+        this.id_quiz = id_quiz;
+    }
+
+
+    public int getId_question() {
+        return id_question;
+    }
+
+    public void setId_question(int id_question) {
+        this.id_question = id_question;
+    }
+
+    public int getId_quiz() {
+        return id_quiz;
+    }
+
+    public void setId_quiz(int id_quiz) {
+        this.id_quiz = id_quiz;
+    }
+    
+    
 }
