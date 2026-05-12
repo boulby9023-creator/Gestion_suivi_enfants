301ac115bba7f3d57ea2ddc31209dd75504da1a7
Famory_Josue_Sissoko <famoryjosuesissoko@gmail.com>
2026-05-12 08:28:42 +0000

    ajout class capacite

---
+++ src/main/java/Modele/Capacites.java
@@ -0,0 +1,6 @@
+package main.java.Modele;
+
+public class Capacites {
+    private int id_capacite;
+    private String type_capacite;
+}
---
+++ src/main/java/Modele/Questionquiz.java
@@ -0,0 +1,6 @@
+package main.java.Modele;
+
+public class Questionquiz {
+    private int id_question;
+    private int id_quiz;
+}
