c324392f7a3abe80c7ffb98fed41e4c63192aa66
med <seibacly@gmail.com>
2026-05-12 09:49:30 +0000

    crud de la table utilisateur

--- src/main/java/DAO/ImplUtilisateurDao.java
+++ src/main/java/DAO/ImplUtilisateurDao.java
@@ -1,8 +1,7 @@
 package main.java.DAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import main.java.BD.ConnexionDB;
 import main.java.Modele.Utilisateur;
@@ -11,7 +10,7 @@ public class ImplUtilisateurDao implements Repository<Utilisateur, Integer>{
     Connection con = ConnexionDB.getConexion();
     @Override
     public void save(Utilisateur entity) {
-        entity.toString();
+        //entity.toString();
         String sql = "INSERT INTO utilisateurs VALUES(?,?,?,?,?,?,?)";
         try {
             PreparedStatement prepare = con.prepareStatement(sql);
@@ -36,23 +35,98 @@ public class ImplUtilisateurDao implements Repository<Utilisateur, Integer>{
     }
 
     @Override
-    public Utilisateur findById(Integer integer) {
+    public Utilisateur findById(Integer id) {
+        Utilisateur utilisateur = new Utilisateur();
+        String sql = "SELECT * FROM utilisateurs WHERE id = " + id;
+        try {
+            Statement stat = con.createStatement();
+            ResultSet result = stat.executeQuery(sql);
+
+            if (result.next()){
+                utilisateur.setId(result.getInt("id"));
+                utilisateur.setNom(result.getString("nom"));
+                utilisateur.setPrenom(result.getString("prenom"));
+                utilisateur.setTel(result.getString("tel"));
+                utilisateur.setMail(result.getString("mail"));
+                utilisateur.setMotDePasse(result.getString("mot_de_passe"));
+                utilisateur.setRole(result.getString("roles"));
+
+                return utilisateur;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     @Override
     public List<Utilisateur> findAll() {
-        return List.of();
+
+        List <Utilisateur> utilisateurs = new ArrayList();
+        Utilisateur utilisateur = new Utilisateur();
+        String sql = "SELECT * FROM utilisateurs";
+        try {
+            Statement stat = con.createStatement();
+            ResultSet result = stat.executeQuery(sql);
+            while(result.next()){
+                utilisateur.setId(result.getInt("id"));
+                utilisateur.setNom(result.getString("nom"));
+                utilisateur.setPrenom(result.getString("prenom"));
+                utilisateur.setTel(result.getString("tel"));
+                utilisateur.setMail(result.getString("mail"));
+                utilisateur.setMotDePasse(result.getString("mot_de_passe"));
+                utilisateur.setRole(result.getString("roles"));
+
+                utilisateurs.add(utilisateur);
+                utilisateur = new Utilisateur();
+            }
+            return utilisateurs;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void delete(Integer integer) {
+        String sql = "DELETE utilisateurs WHERE id = ?";
+        try {
+            PreparedStatement prepare = con.prepareStatement(sql);
+            prepare.setInt(1, integer);
+
+            int resultat =  prepare.executeUpdate();
+            if (resultat>0){
+                System.out.println("Utilisateur supprimer");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void updtae(Integer id, Utilisateur entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updtae'");
+        String sql = "UPDATE utilisateurs " +
+                     "SET nom = ?, prenom = ?, tel = ?, mail = ?, mot_de_passe = ?, roles = ?" +
+                     "WHERE id = ?";
+
+        try {
+            PreparedStatement prepare = con.prepareStatement(sql);
+            prepare.setString(1, entity.getNom());
+            prepare.setString(2, entity.getPrenom());
+            prepare.setString(3, entity.getTel());
+            prepare.setString(4, entity.getMail());
+            prepare.setString(5, entity.getMotDePasse());
+            prepare.setString(6, entity.getRole());
+            prepare.setInt(7, id);
+            int resultat = prepare.executeUpdate();
+            if (resultat >0){
+                System.out.println("L'utilisateur modifier");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
