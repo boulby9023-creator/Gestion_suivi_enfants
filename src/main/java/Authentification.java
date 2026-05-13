514656376667e75725726e5652c473433a197f38
ibrahima Coulibaly <boulby9023@gmail.com>
2026-05-13 10:04:34 +0000

    Ajout option connexion admin

--- src/main/java/Authentification.java
+++ src/main/java/Authentification.java
@@ -7,9 +7,9 @@ import main.java.Service.interfaces.ConnexionInService;
 import main.java.Service.interfaces.UtilisateurInService;
 
 public class Authentification {
-    private Scanner scanner = new Scanner(System.in);
-    private  UtilisateurInService utilisateurInService = new  UtilisateurImplService();
-    private ConnexionInService connexionInService = new ConnexionService();
+    private final  Scanner scanner = new Scanner(System.in);
+    private final  UtilisateurInService utilisateurInService = new  UtilisateurImplService();
+    private final ConnexionInService connexionInService = new ConnexionService();
 
     public void start() {
         boolean runing = true;
@@ -17,12 +17,17 @@ public class Authentification {
             System.out.println("===== Authentification ======");
             System.out.println("1 Inscription");
             System.out.println("2 connexion");
+            System.out.println("3 connexion en tant que admin");
             int choix = scanner.nextInt();
 
             switch (choix) {
-                case 1: utilisateurInService.menu(); break;
-                case 2: connexionInService.connexion();break;
-                default: 
+                case 1 -> utilisateurInService.menu();
+                case 2 -> connexionInService.connexion();
+                case 3 -> System.out.println("En cours de développement...");
+                default -> { 
+                    System.out.println("Choix invalide. Veuillez réessayer.");
+                    runing = false;
+                }
                     
             }
         }
