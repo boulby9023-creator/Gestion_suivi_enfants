61ddf5ba8675671f7d9a1d6c259206bd953b10f5
Famory_Josue_Sissoko <famoryjosuesissoko@gmail.com>
2026-05-14 16:53:36 +0000

    listeQuiz update

--- src/main/java/Service/ListQuestionsParQuiz.java
+++ src/main/java/Service/ListQuestionsParQuiz.java
@@ -1,9 +1,23 @@
 package main.java.Service;
 
-public class ListQuestionsParQuiz {
+import java.util.List;
 
+import main.java.DAO.ImplQuestionDAO;
+import main.java.Modele.Question;
+
+public class ListQuestionsParQuiz {
+    private ImplQuestionDAO implQuestionDAO;
     public void menu() {
         System.out.println("===== La liste des questions du quiz choisi ======");
+        questions(0);
     }
 
+
+    public void questions(int idQuestion){
+        List<Question> questionsList = implQuestionDAO.recuperationQuestionsByIdQuiz(idQuestion);
+        for(Question question:questionsList){
+            System.out.print(question.getId_questions()+" : "+question.getEnonce());
+        }
+    }
+    
 }
